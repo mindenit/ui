7def13a51b7d118e7d1ae40fd317eaba3bfc3b9d
OneLiL05 <kirillofficial0507@gmail.com>
2024-03-25 17:48:30 +0200

    feat: add input component

---
+++ src/components/TextField/TextFieldInput.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+	<input
+		class="font-regular peer flex w-full items-center border-0 bg-transparent pl-2 text-sm text-black selection:bg-royal-blue-200 focus:outline-none disabled:cursor-not-allowed dark:text-fiord-300 dark:selection:bg-royal-blue-900 dark:placeholder:text-fiord-500 [&:where([type='number'])]:[appearance:textfield]"
+	/>
+</template>
