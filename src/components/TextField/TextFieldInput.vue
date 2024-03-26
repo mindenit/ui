652a15655a84fa9e4bf4c57856082aaa8f0750bf
OneLiL05 <kirillofficial0507@gmail.com>
2024-03-26 21:25:36 +0200

    fix: cleanup

--- src/components/TextField/TextFieldInput.vue
+++ src/components/TextField/TextFieldInput.vue
@@ -1,7 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useVModel } from '@vueuse/core'
+
+const props = defineProps<{
+	modelValue?: string | number
+	defaultValue?: string | number
+}>()
+
+const emits = defineEmits<{
+	'update:modelValue': [payload: string | number]
+}>()
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+	passive: true,
+	defaultValue: props.defaultValue
+})
+</script>
 
 <template>
 	<input
+		v-model="modelValue"
 		class="font-regular peer flex w-full items-center border-0 bg-transparent pl-2 text-sm text-black selection:bg-royal-blue-200 focus:outline-none disabled:cursor-not-allowed dark:text-fiord-300 dark:selection:bg-royal-blue-900 dark:placeholder:text-fiord-500 [&:where([type='number'])]:[appearance:textfield]"
 	/>
 </template>
