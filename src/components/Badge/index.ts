4bd9b4f01689a5f96d37745da510e7046731deef
Roman Trashutin <tttrashutin@gmail.com>
2024-07-19 22:42:21 +0300

    feat: add Badge component

---
+++ src/components/Badge/index.ts
@@ -0,0 +1,23 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+
+export const badgeVariants = cva('', {
+	variants: {
+		color: {
+			default: 'border-fiord-400 text-fiord-400',
+			primary: 'border-royal-blue-400 text-royal-blue-400',
+			success: 'border-christi-400 text-christi-400',
+			danger: 'border-amaranth-400 text-amaranth-400'
+		}
+	},
+	defaultVariants: {
+		color: 'default'
+	}
+})
+
+type BadgeVariants = VariantProps<typeof badgeVariants>
+
+export interface BadgeProps {
+	color: BadgeVariants['color']
+}
+
+export { default as Badge } from './TheBadge.vue'
