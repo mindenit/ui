3e476a50655a50fa41269f84a9f3bd140738e550
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-07-21 23:33:35 +0300

    fix: fix some Accordion styles

--- src/components/Accordion/AccordionItem.vue
+++ src/components/Accordion/AccordionItem.vue
@@ -16,11 +16,11 @@ const forwarded = useForwardProps(props)
 <template>
 	<RadixAccordionItem
 		v-bind="forwarded"
-		class="overflow-hidden rounded-lg border border-fiord-200 text-sm dark:border-fiord-800"
+		class="overflow-hidden rounded-lg border border-fiord-300 text-sm dark:border-fiord-800"
 	>
 		<AccordionHeader class="flex">
 			<AccordionTrigger
-				class="group flex h-[48px] flex-1 cursor-pointer items-center justify-between bg-white px-[14px] font-medium outline-none transition-all hover:bg-fiord-200 data-[state=open]:bg-fiord-200 dark:bg-fiord-900 dark:text-white dark:hover:bg-fiord-800 dark:data-[state=open]:bg-fiord-800"
+				class="group flex h-12 flex-1 cursor-pointer items-center justify-between bg-white px-3 font-medium outline-none transition-all hover:bg-fiord-200 data-[state=open]:bg-fiord-200 dark:bg-fiord-900 dark:text-white dark:hover:bg-fiord-800 dark:data-[state=open]:bg-fiord-800"
 			>
 				<slot name="title"></slot>
 				<Icon
@@ -30,9 +30,9 @@ const forwarded = useForwardProps(props)
 			</AccordionTrigger>
 		</AccordionHeader>
 		<AccordionContent
-			class="overflow-hidden bg-fiord-200 text-fiord-800 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown dark:bg-fiord-800 dark:text-fiord-200"
+			class="box-border overflow-hidden bg-fiord-200 text-fiord-800 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown dark:bg-fiord-800 dark:text-fiord-200"
 		>
-			<div class="px-[14px] py-[6px]">
+			<div class="px-3 pb-3 pt-2">
 				<slot name="content"></slot>
 			</div>
 		</AccordionContent>
