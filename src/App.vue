faabb59ad22ffa5b92d51d2c61541ff152b81beb
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-07-21 23:24:33 +0300

    chore: clean App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-import { SelectRoot, SelectTrigger, SelectContent, SelectItem } from './components/Select'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-	<SelectRoot>
-		<SelectTrigger placeholder="Select option" />
-		<SelectContent>
-			<SelectItem value="option-1">Option 1</SelectItem>
-			<SelectItem value="option-2">Option 2</SelectItem>
-			<SelectItem value="option-3" disabled>Option 3</SelectItem>
-		</SelectContent>
-	</SelectRoot>
+	<div></div>
 </template>
