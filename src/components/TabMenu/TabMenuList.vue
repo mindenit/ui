bd80abec2322671a7512bd222aaee361144901e4
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-07-23 17:05:57 +0300

    fix: fix some styles

--- src/components/TabMenu/TabMenuList.vue
+++ src/components/TabMenu/TabMenuList.vue
@@ -7,7 +7,7 @@ const forwarded = useForwardProps(props)
 <template>
 	<TabsList
 		v-bind="forwarded"
-		class="relative flex border-y border-fiord-200 py-[14px] data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:gap-y-2 data-[orientation=vertical]:rounded-xl data-[orientation=vertical]:border data-[orientation=vertical]:p-[10px] dark:border-fiord-600"
+		class="relative flex border-y border-fiord-200 py-[14px] data-[orientation=horizontal]:h-12 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:gap-y-2 data-[orientation=vertical]:rounded-xl data-[orientation=vertical]:border-0 dark:border-fiord-600"
 	>
 		<TabsIndicator
 			class="absolute bottom-0 left-0 h-[2px] w-[--radix-tabs-indicator-size] translate-x-[--radix-tabs-indicator-position] rounded-full px-8 transition-[width,transform] duration-300"
--- src/components/TabMenu/TabMenuTrigger.vue
+++ src/components/TabMenu/TabMenuTrigger.vue
@@ -8,7 +8,7 @@ const forwarded = useForwardProps(props)
 <template>
 	<TabsTrigger
 		v-bind="forwarded"
-		class="flex flex-1 cursor-pointer select-none items-center justify-center gap-[6px] text-sm text-fiord-400 outline-none transition-all hover:text-black data-[orientation=vertical]:w-full data-[orientation=vertical]:justify-start data-[orientation=vertical]:rounded-md data-[orientation=vertical]:p-2 data-[state=active]:text-black data-[orientation=vertical]:hover:bg-royal-blue-100 dark:text-fiord-400 hover:dark:text-white dark:data-[state=active]:text-white data-[orientation=vertical]:dark:hover:bg-royal-blue-950"
+		class="flex flex-1 cursor-pointer select-none items-center justify-center gap-2 text-sm text-fiord-500 outline-none transition-all hover:text-black data-[orientation=vertical]:w-full data-[orientation=vertical]:justify-start data-[orientation=vertical]:rounded-md data-[state=active]:data-[orientation=vertical]:bg-fiord-100 data-[orientation=vertical]:p-2 data-[state=active]:text-black data-[orientation=vertical]:hover:bg-fiord-100 dark:text-fiord-400 hover:dark:text-white dark:data-[state=active]:data-[orientation=vertical]:bg-fiord-800 dark:data-[state=active]:text-white data-[orientation=vertical]:dark:hover:bg-fiord-800 [&_svg]:data-[state=active]:text-royal-blue-500"
 	>
 		<slot />
 	</TabsTrigger>
