858cfbc4513f866282531ba5c707a68b5bac9bba
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-09-08 01:22:01 +0300

    fix: fix modelValue in SearchField component

--- src/components/SearchField/SearchField.vue
+++ src/components/SearchField/SearchField.vue
@@ -2,20 +2,29 @@
 import { TextFieldRoot, TextFieldSlot, TextFieldInput } from '../TextField'
 import { Icon } from '@iconify/vue'
 import { ref } from 'vue'
-import { useFocus } from '@vueuse/core'
+import { useFocus, useVModel } from '@vueuse/core'
 import { IconButton } from '../IconButton'
 
 const element = ref<HTMLInputElement | null>(null)
-const searchValue = ref('')
+const props = defineProps<{
+	modelValue?: string
+	defaultValue?: string
+}>()
+
+const emits = defineEmits<{
+	'update:modelValue': [payload: string]
+}>()
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+	passive: true,
+	defaultValue: props.defaultValue
+})
+
 const { focused } = useFocus(element)
 
 const handleReset = () => {
-	searchValue.value = ''
+	modelValue.value = ''
 	focused.value = true
-}
-
-const handleUpdate = (value: string | number) => {
-	searchValue.value = value as string
 }
 </script>
 
@@ -24,17 +33,10 @@ const handleUpdate = (value: string | number) => {
 		<TextFieldSlot>
 			<Icon icon="ph:magnifying-glass" />
 		</TextFieldSlot>
-		<TextFieldInput
-			v-bind="$attrs"
-			ref="element"
-			id="search"
-			type="search"
-			:model-value="searchValue"
-			@update:model-value="handleUpdate"
-		/>
+		<TextFieldInput v-bind="$attrs" v-model="modelValue" ref="element" id="search" type="search" />
 		<TextFieldSlot>
 			<IconButton
-				v-if="searchValue.length"
+				v-if="modelValue?.length"
 				icon="ph:x"
 				variant="ghost"
 				size="sm"
