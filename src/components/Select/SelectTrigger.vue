6efd653c589fa40a3beeacf6d3cda605eeb5189a
perkinson1251 <tttrashutin@gmail.com>
2024-09-27 23:59:12 +0300

    fix: width of select component

--- src/components/Select/SelectTrigger.vue
+++ src/components/Select/SelectTrigger.vue
@@ -9,7 +9,7 @@ const props = defineProps<SelectTriggerProps & { placeholder: string }>()
 	<SelectTrigger
 		id="selectTrigger"
 		v-bind="props"
-		class="group inline-flex h-10 w-72 cursor-pointer select-none items-center justify-between rounded-lg border border-fiord-300 px-3 text-sm focus:ring focus:ring-fiord-950/15 dark:border-fiord-700 dark:text-white dark:hover:bg-fiord-900 dark:focus:ring-fiord-50/15 dark:data-[placeholder]:text-fiord-400"
+		class="group inline-flex h-10 min-w-72 cursor-pointer select-none items-center justify-between rounded-lg border border-fiord-300 px-3 text-sm focus:ring focus:ring-fiord-950/15 dark:border-fiord-700 dark:text-white dark:hover:bg-fiord-900 dark:focus:ring-fiord-50/15 dark:data-[placeholder]:text-fiord-400"
 	>
 		<SelectValue :placeholder="props.placeholder" />
 		<Icon class="size-4 group-data-[state=open]:rotate-180" icon="ph:caret-down" />
