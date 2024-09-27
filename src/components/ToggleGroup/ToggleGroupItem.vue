b7e36fa84a43092950f125d5b91c54b72659cf16
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-09-28 00:01:27 +0300

    fix: fix ToggleGroup component styles

--- src/components/ToggleGroup/ToggleGroupItem.vue
+++ src/components/ToggleGroup/ToggleGroupItem.vue
@@ -1,29 +1,20 @@
 <script setup lang="ts">
-import {
-	ToggleGroupItemProps,
-	ToggleGroupItemEmits,
-	ToggleGroupItem,
-	useForwardPropsEmits
-} from 'radix-vue'
-import { ref, computed } from 'vue'
+import { ToggleGroupItem, ToggleGroupItemProps, useForwardPropsEmits } from 'radix-vue'
 
 interface Props extends ToggleGroupItemProps {
-	withIcon: boolean
+	withIcon?: boolean
 }
 
-const props = defineProps<Props>()
+const props = withDefaults(defineProps<Props>(), { withIcon: false })
 const forwerded = useForwardPropsEmits(props)
-
-const withIcon = computed(() => {
-	return props.withIcon ? '' : 'min-w-[100px]'
-})
 </script>
 
 <template>
 	<ToggleGroupItem
 		v-bind="forwerded"
-		class="flex items-center justify-center rounded-md p-1 text-sm text-fiord-400 transition-all data-[state=on]:bg-white data-[state=on]:text-black data-[state=on]:shadow-md dark:data-[state=on]:bg-fiord-800 dark:data-[state=on]:text-white"
-		:class="withIcon"
-		><slot />
+		class="flex items-center justify-center rounded-md p-1 text-sm text-fiord-400 transition-all data-[state=on]:bg-white data-[state=on]:text-black data-[state=on]:shadow-md dark:data-[state=on]:bg-fiord-800 dark:data-[state=on]:text-white [&_svg]:size-4"
+		:class="{ 'h-8 min-w-[100px]': !withIcon, 'size-8': withIcon }"
+	>
+		<slot />
 	</ToggleGroupItem>
 </template>
