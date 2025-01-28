a9a864b23720d6de7ba0900a17245188d63dd8d0
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-01-28 20:50:59 +0200

    chore: add themes addon to storybook

--- packages/mindenit-ui/.storybook/main.ts
+++ packages/mindenit-ui/.storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
+    "@storybook/addon-themes",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
