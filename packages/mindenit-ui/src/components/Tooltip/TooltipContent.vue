53f91989465edbe2ffdec80525268888006364c8
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-01-31 15:05:06 +0200

    fix: fix animations

--- packages/mindenit-ui/src/components/Tooltip/TooltipContent.vue
+++ packages/mindenit-ui/src/components/Tooltip/TooltipContent.vue
@@ -5,7 +5,7 @@ import { TooltipArrow, TooltipContent, TooltipPortal } from 'reka-ui'
 <template>
 	<TooltipPortal>
 		<TooltipContent
-			class="select-none rounded-[6px] bg-white px-[10px] py-1 text-sm text-black shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade dark:bg-fiord-900 dark:text-white"
+			class="select-none rounded-[6px] bg-white px-[10px] py-1 text-sm text-black shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slide-up-and-fade data-[state=delayed-open]:data-[side=left]:animate-slide-right-and-fade data-[state=delayed-open]:data-[side=right]:animate-slide-left-and-fade data-[state=delayed-open]:data-[side=top]:animate-slide-down-and-fade dark:bg-fiord-900 dark:text-white"
 			:side-offset="5"
 		>
 			<slot />
