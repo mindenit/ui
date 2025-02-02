7c64134f8b37db37c0719c934768eb2234c9da41
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-02-02 14:53:47 +0200

    fix: fix SelectItem layout

--- packages/mindenit-ui/src/components/Select/SelectItem.vue
+++ packages/mindenit-ui/src/components/Select/SelectItem.vue
@@ -8,9 +8,9 @@ const props = defineProps<SelectItemProps>()
 <template>
 	<SelectItem
 		v-bind="props"
-		class="text-fiord-950 inline-flex h-7 min-w-32 cursor-pointer select-none items-center [data-highlighted]:bg-fiord-100 rounded-lg px-2 text-sm outline-none hover:bg-fiord-100 data-[state=checked]:bg-fiord-100 dark:text-fiord-50 dark:hover:bg-fiord-800 dark:data-[state=checked]:bg-fiord-800 transition"
+		class="text-fiord-950 pl-7 inline-flex h-7 min-w-32 cursor-pointer select-none items-center [data-highlighted]:bg-fiord-100 rounded-lg px-2 text-sm outline-none hover:bg-fiord-100 data-[state=checked]:bg-fiord-100 dark:text-fiord-50 dark:hover:bg-fiord-800 dark:data-[state=checked]:bg-fiord-800 transition"
 	>
-		<SelectItemIndicator>
+		<SelectItemIndicator class="absolute left-4">
 			<Icon class="size-4" icon="lucide:check" />
 		</SelectItemIndicator>
 		<SelectItemText><slot /></SelectItemText>
--- packages/mindenit-ui/src/components/Select/SelectTrigger.vue
+++ packages/mindenit-ui/src/components/Select/SelectTrigger.vue
@@ -9,7 +9,7 @@ const props = defineProps<SelectTriggerProps & { placeholder: string }>()
 	<SelectTrigger
 		id="selectTrigger"
 		v-bind="props"
-		class="dark:hover:border-fiord-600 dark:text-fiord-50 dark:hover:data-[placeholder]:text-fiord-400 group inline-flex hover:border-fiord-400  hover:data-[placeholder]:text-fiord-600 transition h-10 min-w-52 data-[placeholder]:text-fiord-500 text-fiord-950 cursor-pointer select-none items-center justify-between rounded-xl border border-fiord-300 px-3 text-sm focus:ring-3 focus:ring-fiord-400/50 focus:data-[placeholder]:text-fiord-600 focus:outline-none dark:border-fiord-700 dark:data-[placeholder]:text-fiord-500  dark:focus:ring-fiord-600/50 dark:focus:border-fiord-600 dark:focus:data-[placeholder]:text-fiord-400"
+		class="dark:hover:border-fiord-600 dark:text-fiord-50 dark:hover:data-[placeholder]:text-fiord-400 group inline-flex hover:border-fiord-400  hover:data-[placeholder]:text-fiord-600 transition h-10 min-w-52 data-[placeholder]:text-fiord-500 text-fiord-950 cursor-pointer select-none items-center justify-between rounded-lg border border-fiord-300 px-3 text-sm focus:ring-3 focus:ring-fiord-400/50 focus:data-[placeholder]:text-fiord-600 focus:outline-none dark:border-fiord-700 dark:data-[placeholder]:text-fiord-500  dark:focus:ring-fiord-600/50 dark:focus:border-fiord-600 dark:focus:data-[placeholder]:text-fiord-400"
 	>
 		<SelectValue :placeholder="props.placeholder" />
 		<Icon class="size-4 group-data-[state=open]:rotate-180 transition" icon="lucide:chevron-down" />
