dcb52686d5b8fe35ab31b145f593cb12bbb1d92f
perkinson1251 <tttrashutin@gmail.com>
2025-02-02 13:33:25 +0200

    refactor: improve select component styles

--- packages/mindenit-ui/src/components/Select/SelectContent.vue
+++ packages/mindenit-ui/src/components/Select/SelectContent.vue
@@ -25,7 +25,7 @@ const forwarded = useForwardPropsEmits(props, emits)
 			v-bind="forwarded"
 			:class="
 				cn(
-					'z-50 box-border w-full rounded-xl border border-fiord-300 shadow-[0px_4px_6px_-1px_#67738880] bg-white p-2 dark:border-fiord-700 dark:bg-fiord-950',
+					'z-50 box-border w-full rounded-xl border border-fiord-300 shadow-md shadow-fiord-500/50 bg-white p-2 dark:border-fiord-700 dark:bg-fiord-950',
 					'data-[state=open]:animate-slide-down-and-fade',
 					'data-[state=closed]:animate-slide-up-and-fade',
 					props.class,
--- packages/mindenit-ui/src/components/Select/SelectItem.vue
+++ packages/mindenit-ui/src/components/Select/SelectItem.vue
@@ -8,11 +8,11 @@ const props = defineProps<SelectItemProps>()
 <template>
 	<SelectItem
 		v-bind="props"
-		class="text-fiord-950 inline-flex h-9 min-w-32 cursor-pointer select-none items-center justify-between rounded-lg px-2 text-sm outline-none hover:bg-fiord-100 dark:text-fiord-50 dark:hover:bg-fiord-800 transition"
+		class="text-fiord-950 inline-flex h-7 min-w-32 cursor-pointer select-none items-center [data-highlighted]:bg-fiord-100 rounded-lg px-2 text-sm outline-none hover:bg-fiord-100 data-[state=checked]:bg-fiord-100 dark:text-fiord-50 dark:hover:bg-fiord-800 dark:data-[state=checked]:bg-fiord-800 transition"
 	>
-		<SelectItemText><slot /></SelectItemText>
 		<SelectItemIndicator>
 			<Icon class="size-4" icon="lucide:check" />
 		</SelectItemIndicator>
+		<SelectItemText><slot /></SelectItemText>
 	</SelectItem>
 </template>
